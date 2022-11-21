cdf4d2d986f3037c3669e584698557c497f853b9
Vinicius Araujo <sebastianbavini@hotmail.com>
2022-11-21 18:39:25 -0300

    Atribuivel

--- app/src/main/java/com/codecomponents/smartgoalsa/AtribuivelActivity.kt
+++ app/src/main/java/com/codecomponents/smartgoalsa/AtribuivelActivity.kt
@@ -2,9 +2,10 @@ package com.codecomponents.smartgoalsa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.codecomponents.smartgoalsa.databinding.ActivityAtribuivelBinding
 
-class AtribuivelActivity : AppCompatActivity() {
+class AtribuivelActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityAtribuivelBinding
     private lateinit var estalo:String
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,10 @@ class AtribuivelActivity : AppCompatActivity() {
         binding=ActivityAtribuivelBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.textViewEstalo.text=intent.getStringExtra("estalo").toString()
+        binding.buttonAvancar3De5.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
 
     }
 }
