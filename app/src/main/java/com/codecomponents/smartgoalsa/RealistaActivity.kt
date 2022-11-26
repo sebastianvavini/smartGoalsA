fb5e2de72ec718d57b42bb3b9b4325c24205934e
sebastianbavini <sebastianbavini@hotmail.com>
2022-11-26 17:46:28 -0300

    Adicionada Activity Realista

--- app/src/main/java/com/codecomponents/smartgoalsa/RealistaActivity.kt
+++ app/src/main/java/com/codecomponents/smartgoalsa/RealistaActivity.kt
@@ -2,10 +2,15 @@ package com.codecomponents.smartgoalsa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codecomponents.smartgoalsa.databinding.ActivityRealistaBinding
 
 class RealistaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRealistaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_realista)
+        binding =ActivityRealistaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+
     }
 }
