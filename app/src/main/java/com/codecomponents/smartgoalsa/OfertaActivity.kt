f54a7f7deaa8c07bed415f66f8d70792d0d6bf51
Sebastiao Vinícius Araújo de Moura <sebastianbavini@hotmail.com>
2023-01-30 18:54:43 -0300

    Lembrar de adicionar recorrencia de pagamentos no caso da oferta

--- app/src/main/java/com/codecomponents/smartgoalsa/OfertaActivity.kt
+++ app/src/main/java/com/codecomponents/smartgoalsa/OfertaActivity.kt
@@ -2,10 +2,21 @@ package com.codecomponents.smartgoalsa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codecomponents.smartgoalsa.databinding.ActivityOfertaBinding
 
 class OfertaActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityOfertaBinding
+    private lateinit var textoEstalo:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_oferta)
+
+        binding= ActivityOfertaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        textoEstalo= intent.getStringExtra("estalo").toString()
+        binding.textEstaloOferta.text=textoEstalo
+
     }
 }
